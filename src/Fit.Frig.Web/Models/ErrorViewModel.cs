26cc95c52ca29ac7b4da6f07f756ebf502f8c3b1
Matthew Schmirler <mschmirler13@alumni.uwosh.edu>
2018-10-17 15:43:24 -0500

    chore: init proj setup from template

---
+++ src/Fit.Frig.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fit.Frig.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
